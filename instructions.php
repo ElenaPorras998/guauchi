d5b506213b3bf41d53a5db0fde7743dbce9d9124
Daniel Palacios <dptorri@gmail.com>
2017-10-13 15:08:39 +0200

    instructions

---
+++ instructions.php
@@ -0,0 +1,24 @@
+<div class="instructions">
+    <h1>BLACK JACK</h1>
+    <h2> (GUAUCHI STYLE)</h2>
+    
+    <h3>How to play:</h3>
+    <ol>     
+        <li>J,Q,K count as 10</li>
+        <li>A can be 1 or 10</li>
+        <li>Don't go over 21</li>
+        <li>That's it!</li>
+    </ol>
+    <h3>Developed by:</h3>
+    <ul>
+        <li>Eddi</li>
+        <li>Elena</li>
+        <li>Daniel</li>
+    </ul>
+    <h3>Follow us on:</h3>
+    <ul>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Github</li>
+    </ul>
+    </div>
